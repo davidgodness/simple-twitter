6787f4458b3e17fdac859b6b829ac9936b5cd2d9
davidchang <daaniloveyou@gmail.com>
2023-07-27 15:45:42 +0800

    simplify controller advice

--- src/main/java/com/davidgodness/simpletwitter/user/UserControllerAdvice.java
+++ src/main/java/com/davidgodness/simpletwitter/user/UserControllerAdvice.java
@@ -1,16 +1,20 @@
 package com.davidgodness.simpletwitter.user;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-public class UserNotFoundAdvice {
-    @ResponseBody
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+@RestControllerAdvice
+public class UserControllerAdvice {
+    @ExceptionHandler(UserExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    String userExistHandler(UserExistException e) {
+        return e.getMessage();
+    }
+
     @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     String userNotFoundHandler(UserNotFoundException e) {
         return e.getMessage();
     }
